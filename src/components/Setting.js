5609b45dfc5d2de5b012f4a15fa267e3f912cf46
vkchuong <vkchuong@uci.edu>
2020-03-30 18:51:59 -0700

    minor bug

--- src/components/Setting.js
+++ src/components/Setting.js
@@ -13,16 +13,16 @@ class Setting extends React.Component {
     }
 
     handleClick = cityName => e => {
-        var i, tabcontent, tablinks;
-        tabcontent = document.getElementsByClassName("tabcontent");
-        for (i = 0; i < tabcontent.length; i++) {
+        let tabcontent = document.getElementsByClassName("tabcontent");
+        for (let i = 0; i < tabcontent.length; i++) {
             tabcontent[i].style.display = "none";
         }
-        tablinks = document.getElementsByClassName("tablinks");
-        for (i = 0; i < tablinks.length; i++) {
+        let tablinks = document.getElementsByClassName("tablinks");
+        for (let i = 0; i < tablinks.length; i++) {
             tablinks[i].className = tablinks[i].className.replace(" active", "");
         }
         document.getElementById(cityName).style.display = "block";
+        e.currentTarget.className += " active"
     }
 
     render() {
